5000271fdbe770f08f944099582941281cb9861d
Eddy Permana <me.eddypermana@gmail.com>
2021-01-12 22:18:30 +0700

    add invoice controller

--- server/controllers/invoice.js
+++ server/controllers/invoice.js
@@ -2,6 +2,25 @@
 
 const InvoiceModel = require("../models/invoice");
 
-class InvoiceController {}
+class InvoiceController {
+  static add = async (req, res, next) => {
+    try {
+      const { customerName, salesPersonName, notes, products } = req.body;
+
+      const newInvoiceData = {
+        customerName: customerName,
+        salesPersonName: salesPersonName,
+        notes: notes,
+      };
+
+      const invoice = await InvoiceModel.create(newInvoiceData)
+
+      
+
+    } catch (error) {
+      next(error);
+    }
+  };
+}
 
 module.exports = InvoiceController;
