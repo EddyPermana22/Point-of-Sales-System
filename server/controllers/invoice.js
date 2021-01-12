4c23a2a41459872229620dfe949b7685589db2b4
Eddy Permana <me.eddypermana@gmail.com>
2021-01-12 22:47:13 +0700

    finish create add invoice method

--- server/controllers/invoice.js
+++ server/controllers/invoice.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const InvoiceModel = require("../models/invoice");
+const SoldProductModel = require("../models/soldProduct");
 
 class InvoiceController {
   static add = async (req, res, next) => {
@@ -13,10 +14,23 @@ class InvoiceController {
         notes: notes,
       };
 
-      const invoice = await InvoiceModel.create(newInvoiceData)
+      const invoice = await InvoiceModel.create(newInvoiceData);
 
-      
+      const soldProductsData = products.map((product) => {
+        return {
+          invoice: invoice._id,
+          name: product.name,
+          picture: product.picture,
+          quantity: product.quantity,
+          price: product.price,
+        };
+      });
 
+      const soldProduct = await SoldProductModel.insertMany(soldProductsData);
+
+      res.status(200).json({
+        message: "invoice created!",
+      });
     } catch (error) {
       next(error);
     }
