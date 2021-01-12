7f5fd355cb394c8edb3bb418112a980f9207026a
Eddy Permana <me.eddypermana@gmail.com>
2021-01-13 00:24:57 +0700

    add dashboard page

--- client/src/POSApp.js
+++ client/src/POSApp.js
@@ -4,6 +4,8 @@ import { Switch, Route } from "react-router-dom";
 import WebHeader from "./components/WebHeader";
 import Sidebar from "./components/Sidebar";
 
+import Dashboard from "./pages/Dashboard";
+
 const POSApp = () => {
   return (
     <>
@@ -14,9 +16,7 @@ const POSApp = () => {
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <Switch>
               <Route path="/" exact>
-                <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                  <h1 className="h2">Dashboard</h1>
-                </div>
+                <Dashboard />
               </Route>
             </Switch>
           </main>
