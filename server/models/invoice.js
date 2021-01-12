8e051ca914abbc9eca3df09e299457397c9a091b
Eddy Permana <me.eddypermana@gmail.com>
2021-01-12 22:10:45 +0700

    add schema data to the invouce Schema

--- server/models/invoice.js
+++ server/models/invoice.js
@@ -3,7 +3,34 @@
 const mongoose = require("mongoose");
 
 const InvoiceSchema = new mongoose.Schema(
-  {},
+  {
+    customerName: {
+      type: String,
+      required: true,
+      trim: true,
+      uppercase: true,
+      min: 2,
+      max: 50,
+    },
+    salesPersonName: {
+      type: String,
+      required: true,
+      trim: true,
+      uppercase: true,
+      min: 2,
+      max: 50,
+    },
+    notes: {
+      type: String,
+      max: 1000,
+    },
+    status: {
+      type: String,
+      enum: ["published"],
+      required: true,
+      default: "published",
+    },
+  },
   { versionKey: false, timestamps: true }
 );
 
