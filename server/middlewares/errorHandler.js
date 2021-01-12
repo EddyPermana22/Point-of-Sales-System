88e16edbdc94303f743dffa63469aa306fec43e4
Eddy Permana <me.eddypermana@gmail.com>
2021-01-12 22:40:10 +0700

    add some logic on error handler middleware

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -5,8 +5,41 @@ const errorHandler = (err, req, res, next) => {
     console.log(err);
   }
 
-  res.status(err.code || 500).json({
-    message: err.message || "internal server error",
+  const customError = {
+    code: 500,
+    message: "",
+  };
+
+  switch (err.name) {
+    case "CastError":
+      customError.code = 404;
+      customError.message = `data not found`;
+      break;
+    case "LoginError":
+      customError.code = err.code || 400;
+      customError.message =
+        err.message || `invalid email / phone & password combination`;
+      break;
+    case "CustomerAuthentication":
+      customError.code = err.code || 401;
+      customError.message = err.message || `authentication error`;
+      break;
+    case "JsonWebTokenError":
+      customError.code = err.code || 401;
+      customError.message = err.message || `invalid access token`;
+      break;
+    case "ResetPasswordError":
+      customError.code = err.code || 400;
+      customError.message =
+        err.message || `reset password failed, please try again`;
+      break;
+    default:
+      customError.code = 500;
+      customError.message = err.message || `internal server error`;
+  }
+
+  res.status(customError.code).json({
+    message: customError.message,
   });
 };
 
